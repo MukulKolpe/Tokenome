db59461f690782ab60d3aa33950d4adacadd1ba9
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-11-30 22:42:42 +0530

    Initial Config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const roboto = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
